07e50cc11ba63e6e357535dfaea8254ff2e74847
Hamza Iftikhar <52419472+hmzaabasi@users.noreply.github.com>
2020-12-12 14:39:04 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import Axios from "axios";
+import CustomTable from "./Components/Table";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  table: {
+    width: "80%",
+    margin: "3rem auto",
+  },
+  heading: {
+    margin: "3rem",
+    color: "#24292e",
+  },
+});
+
+const App = () => {
+  const [tableData, setTableData] = useState([]);
+  const [apiResponse, setApiResponse] = useState([]);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = React.useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const classes = useStyles();
+  const [tableHeaders] = useState([
+    { value: "Name", align: "left" },
+    { value: "Language", align: "right" },
+    { value: "Default Branch", align: "right" },
+    { value: "Open Issues", align: "right" },
+  ]);
+
+  useEffect(() => {
+    async function getRepos() {
+      let response = await Axios.get(
+        "https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100"
+      );
+      setIsLoading(false);
+      const { status, data } = response;
+      if (status === 200 && data?.items?.length) {
+        setApiResponse(
+          data.items.map((item) => ({
+            name: { value: item.full_name, align: "left" },
+            language: { value: item.language, align: "right" },
+            defaultBranch: { value: item.default_branch, align: "right" },
+            openIssues: { value: item.open_issues, align: "right" },
+          }))
+        );
+      }
+    }
+    setIsLoading(true);
+    getRepos();
+  }, []);
+
+  useEffect(() => {
+    setTableData(pageData());
+  }, [apiResponse]);
+
+  useEffect(() => {
+    setTableData(pageData());
+  }, [page, rowsPerPage]);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const pageData = () =>
+    apiResponse.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
+  return (
+    <>
+      <Typography
+        variant="h3"
+        gutterBottom
+        align="center"
+        className={classes.heading}
+      >
+        Github Popular Repositories
+      </Typography>
+      <TableContainer className={classes.table} component={Paper}>
+        <CustomTable
+          th={tableHeaders}
+          tr={tableData}
+          currentPage={page}
+          isLoading={isLoading}
+          totalRows={apiResponse.length}
+          rowsPerPage={rowsPerPage}
+          handlePageChange={handleChangePage}
+          handleChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </TableContainer>
+    </>
+  );
+};
+export default App;
---
+++ src/Components/Table.js
@@ -0,0 +1,101 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  withStyles,
+} from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
+import Pagination from "./Pagination";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: "#24292e",
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const CustomTable = ({
+  th,
+  tr,
+  totalRows,
+  isLoading,
+  rowsPerPage,
+  currentPage,
+  handlePageChange,
+  handleChangeRowsPerPage,
+}) => {
+  return (
+    <>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {th.map((item, index) => (
+              <StyledTableCell key={index} align={item.align}>
+                {item.value}
+              </StyledTableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {!isLoading ? (
+            tr.length ? (
+              tr.map((tds, trKey) => (
+                <TableRow key={trKey}>
+                  {Object.keys(tds).map((td, tdKey) => {
+                    const data = tds[td];
+                    return (
+                      <TableCell key={tdKey} align={data.align}>
+                        {data.value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={4} align="center">
+                  No data
+                </TableCell>
+              </TableRow>
+            )
+          ) : (
+            <TableRow>
+              <TableCell colSpan={4} align="center">
+                <Skeleton height={50} />
+                <Skeleton height={50} animation={false} />
+                <Skeleton height={50} animation="wave" />
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              colSpan={3}
+              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+              count={totalRows}
+              rowsPerPage={rowsPerPage}
+              page={currentPage}
+              SelectProps={{
+                inputProps: { "aria-label": "rows per page" },
+                native: true,
+              }}
+              onChangePage={handlePageChange}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              ActionsComponent={Pagination}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </>
+  );
+};
+
+export default CustomTable;
